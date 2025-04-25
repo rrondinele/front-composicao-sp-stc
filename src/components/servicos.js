3cfb8787d78d937fdd754fcc889c7de56967fe31
Ceneged Supervisão - SP <rondinelerac@gmail.com>
2025-04-25 10:17:42 -0300

    lk

--- src/components/servicos.js
+++ src/components/servicos.js
@@ -1,6 +1,7 @@
 export const servicoOptions = [
     { value: "AFERICAO", label: "AFERICAO" },
     { value: "CORTE", label: "CORTE" },
+    { value: "EMERGENCIA", label: "EMERGENCIA" },
     { value: "LIGACAO NOVA", label: "LIGACAO NOVA" },
     { value: "PROJETO RAMAL", label: "PROJETO RAMAL" },
     { value: "PRE-VENDA", label: "PRE-VENDA" },
