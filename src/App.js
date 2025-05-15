0616486a1e01594f511689dcdff347ae4e4a9adc
Ceneged Supervisão - SP <rondinelerac@gmail.com>
2025-05-15 13:24:16 -0300

    des

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
     const hoje = new Date();
     const dataAtividade = new Date(dataString);
     const diffEmDias = (hoje - dataAtividade) / (1000 * 60 * 60 * 24);
-    return diffEmDias > 2;
+    return diffEmDias > 200;
   };
 
   // Mapeamento de matrícula para nome do supervisor
