abe1e577d498a0d7986bc8598afdb04e5e55de61
Ceneged Supervisão - SP <rondinelerac@gmail.com>
2025-06-10 19:02:31 -0300

    Corrige caminhos de imagens após mover para pages

--- src/pages/CadastroEquipe.jsx
+++ src/pages/CadastroEquipe.jsx
@@ -8,10 +8,14 @@ import React, { useState,useEffect } from "react";
 import axios from "axios";
 import Select from "react-select";
 import * as XLSX from "xlsx";
-import cenegedtIcon from "./ceneged.png"; // Importando o ícone ceneged
-import exitIcon from "./exit.png"; // Importando o ícone logout
-import clearIcon from "./clear.png"; // Importando o ícone Clear
-import excelIcon from "./excel.png"; // Importando o ícone excel
+//import cenegedtIcon from "./ceneged.png"; // Importando o ícone ceneged
+import cenegedtIcon from "../ceneged.png";
+//import exitIcon from "./exit.png"; // Importando o ícone logout
+import exitIcon from "../exit.png";
+//import clearIcon from "./clear.png"; // Importando o ícone Clear
+import clearIcon from "../clear.png";
+//import excelIcon from "./excel.png"; // Importando o ícone excel
+import excelIcon from "../excel.png";
 import { toast, ToastContainer } from "react-toastify"; // Para feedback visual
 import "react-toastify/dist/ReactToastify.css"; // Estilos do toast
 import { supervisorOptions } from "./components/supervisor";
