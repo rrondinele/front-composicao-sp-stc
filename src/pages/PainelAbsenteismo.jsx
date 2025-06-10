48ad9a34ce08bb507e017fbcc49092f4c1183206
Ceneged Supervisão - SP <rondinelerac@gmail.com>
2025-06-10 20:17:11 -0300

    ert

--- src/pages/PainelAbsenteismo.jsx
+++ src/pages/PainelAbsenteismo.jsx
@@ -22,7 +22,7 @@ export default function PainelAbsenteismo() {
   useEffect(() => {
     async function fetchDados() {
       try {
-        const res = await fetch(`${BASE_URL}/teams?data=${data}`);
+        const res = await fetch(`${BASE_URL}/teams/finalizados?data=${data}`);
         const json = await res.json();
         setDados(json);
       } catch (err) {
