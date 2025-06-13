2ce53bed02c4e6618f40de63a20ea4db62528706
Ceneged Supervisão - SP <rondinelerac@gmail.com>
2025-06-13 12:37:34 -0300

    pain

--- src/components/TabelaTeste.jsx
+++ src/components/TabelaTeste.jsx
@@ -18,8 +18,8 @@ export default function TabelaTeste() {
           <tr className="text-sm">
             <td className="p-2 border">Ana Silva</td>
             <td className="p-2 border">Equipe A</td>
-            <td className="p-2 border">Carlos Oliveira</td>
-            <td className="p-2 border">Fernanda Costa</td>
+            <td className="p-2 border">Carlos Oliveira de Silva Souza Alves Parceiro</td>
+            <td className="p-2 border">Fernanda Costa Albuquerque Silva Costa</td>
             <td className="p-2 border">Instalação</td>
             <td className="p-2 border">ABC-1234</td>
             <td className="p-2 border text-green-500">Concluído</td>
