e69624297a4d146c1f0e210d19f7e188e9b052bf
Ceneged Supervisão - SP <rondinelerac@gmail.com>
2026-02-20 18:00:59 -0300

    vc

--- src/components/DateRangeModalSelector.jsx
+++ src/components/DateRangeModalSelector.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { DateRange } from "react-date-range";
-import { format } from "date-fns";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 
